91320741bece5480ea6d8a66a9dd62635b2e5b49
221108 <221108@WINDOWS-NAG4KHA.mr.hi-joho.ac.jp>
2025-05-13 10:21:31 +0900

    new project

--- src/main/java/servlet/LoginServlet.java
+++ src/main/java/servlet/LoginServlet.java
@@ -3,32 +3,55 @@ package servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class LoginServlet
  */
-@WebServlet("/LoginServlet")
+@WebServlet("/Login")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
      
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/login.jsp");
+		dispatcher.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String sId = request.getParameter("ID");
-		String sPassword = request.getParameter("password");
-		
-		response.setContentType("text/html; charset=UTF-8");
+		String nextForward = "WEB-INF/jsp/login.jsp";
+		try {
+			
+			request.setCharacterEncoding("UTF-8");
+			String sId = request.getParameter("ID");
+			String sPassword = request.getParameter("password");
+			
+			HttpSession session = request.getSession();
+			session.setAttribute("ID", sId);
+			session.setAttribute("password", sPassword);
+			
+			
+			if(sId.length() !=0) {
+				nextForward = "WEB-INF/jsp/login.jsp";
+				
+			}
+			
+			
+			
+			
+		}
+		finally {
+			RequestDispatcher dispatcher = request.getRequestDispatcher(nextForward);
+			dispatcher.forward(request, response);
+		}
 		PrintWriter out = response.getWriter();
 		out.println();
 	}
---
+++ src/main/webapp/WEB-INF/jsp/addminHome.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Addmin</title>
+
+<link href="main.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="loginBox">
+	<h1>Login</h1>
+	<form action="Login" method="post" >
+		UserID:<input type="text"id="input1" name="id"><br>	
+		Password:<input type="text"id="input2" name="password"><br>
+		<input type="submit" value="Login">
+	</form>
+</div>
+
+</body>
+</html>
